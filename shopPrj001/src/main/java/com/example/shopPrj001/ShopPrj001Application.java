4fe682ac95ead73693224e901c4a6fd254476d47
Park Saehee <saehee0427@gmail.com>
2024-05-28 12:33:01 +0900

    개인 프로젝트 기초 설정

---
+++ shopPrj001/src/main/java/com/example/shopPrj001/ShopPrj001Application.java
@@ -0,0 +1,13 @@
+package com.example.shopPrj001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopPrj001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopPrj001Application.class, args);
+	}
+
+}
---
+++ shopPrj001/src/test/java/com/example/shopPrj001/ShopPrj001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopPrj001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopPrj001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
